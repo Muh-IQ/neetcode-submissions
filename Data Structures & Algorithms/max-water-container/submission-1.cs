1aa4d1844393c0c4e25db00d393560ba60f4862b
Muh-IQ <mohamedajaj0007@gmail.com>
2026-04-27 21:39:00 +0300

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int MaxArea(int[] arr) {
+        int left = 0 , right = arr.Length-1;
+        int max = 0;
+        while(left < right)
+        {
+            int minHight = Math.Min(arr[left] , arr[right]);
+            int space = 0;
+            space = right - left ;
+            if(arr[left] == minHight)
+            {
+                left++;
+            }
+            else if(arr[right] == minHight){
+                right--;
+            }
+            
+
+            max = Math.Max(max , minHight * space);
+
+        }
+        return max;
+
+    }
+}
