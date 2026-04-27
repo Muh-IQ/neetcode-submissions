c0421f2d85985156e793c9a9990c9b2ae0150b2d
Muh-IQ <mohamedajaj0007@gmail.com>
2026-04-27 11:34:33 +0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int , int> VIR = new Dictionary<int , int>();
+        VIR[nums[0]] = 0;
+        for(int i = 1; i < nums.Length;i++)
+        {
+            int diff = target - nums[i];
+            if(VIR.ContainsKey(diff))
+                return new int[]{VIR[diff],i};
+                
+            VIR[nums[i]] = i;
+        }
+
+        return new int[]{-1,-1};
+    }
+}
