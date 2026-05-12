ace3d94f4df31b1fd03276b1ac3f913beb5af8ac
Muh-IQ <mohamedajaj0007@gmail.com>
2026-05-13 02:42:24 +0300

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        int left = 0;
+        HashSet<int> win = new();
+        for(int right = 0; right < nums.Length; right++)
+        {
+            if(win.Contains(nums[right]))
+                return true;
+            if(win.Count >= k)
+            {
+                win.Remove(nums[left]);
+                left++;
+            }
+            win.Add(nums[right]);
+        }
+
+        return false;
+    }
+}
